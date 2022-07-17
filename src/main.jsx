937b67cbc222f6781d550d0d190ad31b5dfe9ca4
Bruno <brunonoriller@gmail.com>
2022-07-17 09:08:17 -0300

    feat: adding github link

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,14 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div style={{ position: 'absolute' }}>
+      <a
+        href='https://github.com/Noriller/refreshing-way-test'
+        target='_blank'
+        style={{ color: 'white', fontSize: '2em' }}>
+        The repo for this project
+      </a>
+    </div>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
